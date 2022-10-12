1c256dbab8e9444577fe59623cb50a882ec43756
Felipe Soares <soares.felipe200@gmail.com>
2022-10-12 13:23:39 -0300

    incluindo javascript

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,10 +6,42 @@ function get(url){
 }
 
 function criarLinha(usuario){
+    console.log(usuario)
+    linha = document.createElement("tr")
+    tdCpf = document.createElement("td")
+    tdNome = document.createElement("td")
+    tdId = document.createElement("td")
+    tdEmail = document.createElement("td")
+    tdTelefone = document.createElement("td")
+    tdDataNascimento = document.createElement("td")
+
+    tdCpf.innerHTML = usuario.cpf
+    tdNome.innerHTML = usuario.nome
+    tdId.innerHTML = usuario.id
+    tdEmail.innerHTML = usuario.email
+    tdTelefone.innerHTML = usuario.telefone
+    tdDataNascimento.innerHTML = usuario.dataNascimento  
+
+    linha.appendChild(tdId)
+    linha.appendChild(tdNome)
+    linha.appendChild(tdCpf)
+    linha.appendChild(tdEmail)
+    linha.appendChild(tdTelefone)
+    linha.appendChild(tdDataNascimento)
+
+    return linha;
 
 }
 
 function main(){
-    usuarios = get("http://127.0.0.1:8080/clientes")
-    console.log(usuarios)
+    data = get("http://127.0.0.1:8080/clientes")
+    usuarios = JSON.parse(data)
+
+    let tabela = document.getElementById("tabela")
+    usuarios.forEach(element => {
+        let linha = criarLinha(element)
+        tabela.appendChild(linha)
+    })
 }
+
+main()
