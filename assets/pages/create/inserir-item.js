aeca487eb753ee728aa7ece6bd98026f7c29cf17
Felipe <soares.felipe200@gmail.com>
2022-10-28 16:26:58 -0300

    refatorando codigo

--- assets/pages/create/inserir-item.js
+++ assets/pages/create/inserir-item.js
@@ -1,4 +1,11 @@
-function criarLinha(){
+function get(url) {
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
+
+function criarLinha(produto){
     linha = document.createElement("tr")
     tdId = document.createElement("td")
     tdNome = document.createElement("td")
@@ -6,42 +13,30 @@ function criarLinha(){
     tdValorUnitario = document.createElement("td")
     tdSubTotal = document.createElement("td")
 
-    let select = document.getElementById('combo-produto')
-    let option = select.children[select.selectedIndex]
-
-
-    let nomeProduto = option.textContent
-    let quantidadeProduto = document.getElementById('qtd')
-    let valorUnitarioProduto = document.getElementById('vlr-unitario')
-    let subTotal = document.getElementById('vlr-final')
-
-    console.log(select.value)
-    console.log(nomeProduto)
-    console.log(quantidadeProduto.value)
-    console.log(valorUnitarioProduto.value)
-    console.log(subTotal.value)
-
-    tdId.innerHTML = select.value
-    tdNome.innerHTML = nomeProduto
-    tdQuantidade.innerHTML = quantidadeProduto.value
-    tdValorUnitario.innerHTML = valorUnitarioProduto.value
-    tdSubTotal.innerHTML = subTotal.value
+    tdId.innerHTML = produto.id
+    tdNome.innerHTML = produto.nome
+    tdQuantidade.innerHTML = document.getElementById('qtd').value
+    tdValorUnitario.innerHTML = produto.valorUnitario
+    tdSubTotal.innerHTML = document.getElementById('vlr-final').value
 
     linha.appendChild(tdId)
     linha.appendChild(tdNome)
     linha.appendChild(tdQuantidade)
     linha.appendChild(tdValorUnitario)
     linha.appendChild(tdSubTotal)
-    
     linha.setAttribute("class", "row")
 
     return linha;
-
 }
 
 function inserirItem(){
     let tabela = document.getElementById("tabela-item")
-    let linha = criarLinha()
+    let select = document.getElementById('combo-produto')
+    let id = select.value
+    data = get(`http://127.0.0.1:8080/produtos/${id}`)
+    let produto = JSON.parse(data)
+
+    let linha = criarLinha(produto)
     tabela.appendChild(linha)
     
 }
