8157ddce516495bc03abeb5d2b944413289e80d8
Felipe <soares.felipe200@gmail.com>
2022-11-19 08:32:03 -0300

    incluindo funcao de remover item

--- assets/pages/create/inserir-item.js
+++ assets/pages/create/inserir-item.js
@@ -5,7 +5,19 @@ function get(url) {
     return request.responseText
 }
 
-function criarLinha(produto){
+function marcarCheckBox(id,el) {
+    let checkBox = document.getElementById(id)
+    if (!checkBox.checked) {
+        checkBox.checked = true
+        el.setAttribute('style','background-color:#93a8ac;')
+    }
+    else {
+        checkBox.checked = false
+        el.removeAttribute('style')
+    }
+}
+
+function criarLinha(produto, idCheck) {
     linha = document.createElement("tr")
     tdId = document.createElement("td")
     tdNome = document.createElement("td")
@@ -16,7 +28,7 @@ function criarLinha(produto){
     let quantidade = document.getElementById('qtd')
     let valorTotal = document.getElementById('vlr-final')
 
-    if(quantidade.value < 1){
+    if (quantidade.value < 1) {
         alert('Informe a quantidade!')
         return -1
     }
@@ -32,19 +44,41 @@ function criarLinha(produto){
     linha.appendChild(tdQuantidade)
     linha.appendChild(tdValorUnitario)
     linha.appendChild(tdSubTotal)
-    linha.setAttribute("class", "row")
-
-    return linha;
+    linha.setAttribute("class", "row" + idCheck)
+    return linha
 }
+var cont = 0;
+function inserirItem() {
 
-function inserirItem(){
-    let tabela = document.getElementById("tabela-item")
+    let tabelaBody = document.getElementById("body-tabela")
     let select = document.getElementById('combo-produto')
     let id = select.value
     data = get(`http://127.0.0.1:8080/product/${id}`)
     let produto = JSON.parse(data)
-    let linha = criarLinha(produto)
-    if(linha != -1)
-        tabela.appendChild(linha)
-    
+
+    let checkBox = document.createElement('input')
+    checkBox.type = 'checkbox'
+    checkBox.id = id + '-chk' + cont
+    checkBox.setAttribute('style','display: none;')
+
+
+    let linha = criarLinha(produto, checkBox.id)
+
+
+
+    if (linha != -1) {
+        linha.setAttribute('onclick', `marcarCheckBox('${checkBox.id}',this)`)
+        tabelaBody.appendChild(checkBox)
+        tabelaBody.appendChild(linha)
+        cont += 1
+    }
+
+}
+
+function removerItem() {
+    $("input:checked").each(function () {
+        console.log($(this).attr("id"));
+        $(`[id=${$(this).attr("id")}]`).remove();
+        $(`[class='row${$(this).attr("id")}']`).remove();
+    });
 }
