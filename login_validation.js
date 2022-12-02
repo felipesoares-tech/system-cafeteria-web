eda3a8fa0d40c9f99438fe37462e0181d0ceae8c
Felipe <soares.felipe200@gmail.com>
2022-12-02 18:02:35 -0300

    atualizando diretorios

--- login_validation.js
+++ login_validation.js
@@ -10,13 +10,13 @@ let submit = document.getElementById('btn-login')
 var chk = 0
 
 
-function logar(){
+function logar(){ //Function para login no sistema
     
-    let login = document.getElementById('mail').value
-    let senha = document.getElementById('senha').value
-    let listaUser = []
+    let login = document.getElementById('mail').value // recupera o login informado pelo usuario
+    let senha = document.getElementById('senha').value // recupera a senha informada pelo usuario
+    let listaUser = [] // Lista para armazenar todos os usuários já existentes
 
-    let userValid = {
+    let userValid = { //objeto para validar o usuário que ficará logado.
         email: '',
         nome:'',
         id:'',
@@ -25,10 +25,9 @@ function logar(){
 
     data = get("http://127.0.0.1:8080/attendant")
     listaUser = JSON.parse(data)
-    console.log(listaUser)
-
+    
     listaUser.forEach((item) =>{
-        if(login == item.email && senha == item.senha){
+        if(login == item.email && senha == item.senha){ // caso encontre o login e senha do usuario na lista de usuarios, iremos recuperar o objeto
             userValid = {
                 email: item.email,
                 nome: item.nome,
@@ -39,7 +38,7 @@ function logar(){
         }
     })
 
-    if(login == userValid.email && senha == userValid.senha && chk == 1){
+    if(login == userValid.email && senha == userValid.senha && chk == 1){  // se o login e senha
         window.location.href = 'http://127.0.0.1:5500/index.html'
         let token = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2)
         localStorage.setItem('token',token)
